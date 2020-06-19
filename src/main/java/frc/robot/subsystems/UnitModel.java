3de308e6b30bfd0b3172fb69a6f91e619602b0ad
Unknown <orel.david1502@gmail.com>
2020-06-19 20:51:37 +0300

    Add unitModel

---
+++ src/main/java/frc/robot/subsystems/UnitModel.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems;
+
+public class UnitModel {
+    double ticksPerUnit;
+
+    public UnitModel(double ticksPerUnit) {
+        this.ticksPerUnit = ticksPerUnit;
+    }
+
+    public double toUnits(double ticks) {
+        return ticks / ticksPerUnit;
+    }
+
+    public int toTicks(double units) {
+        return (int) (ticksPerUnit * units);
+    }
+
+    public double toVelocity(double ticks100ms){
+        return (ticks100ms / ticksPerUnit) * 10;
+    }
+
+    public int toTicks100ms(double velocity){
+        return (int) (velocity * ticksPerUnit / 10);
+    }
+
+}
