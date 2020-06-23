3b921325af7f8e6cd2ef88446a647e3bae888851
Barel Shlidor <barel@shlidor.org>
2020-06-23 16:17:28 +0300

    Expand the constant annotation to work with RangeConstant

--- src/main/java/frc/robot/subsystems/base/common/Constant.java
+++ src/main/java/frc/robot/subsystems/base/common/Constant.java
@@ -1,9 +1,6 @@
 package frc.robot.subsystems.base.common;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
@@ -11,4 +8,5 @@ public @interface Constant {
     double doubleVal() default 0.0;
     int intVal() default 0;
     long longVal() default 0L;
+    RangeConstant rangeVal() default @RangeConstant();
 }
