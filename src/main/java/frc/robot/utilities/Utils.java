79d3f9fe3853b9dc286b192fba3017bd1fe89c94
Barel Shlidor <barel@shlidor.org>
2020-06-23 16:48:57 +0300

    Remove unused methods from utils class

--- src/main/java/frc/robot/utilities/Utils.java
+++ src/main/java/frc/robot/utilities/Utils.java
@@ -4,11 +4,6 @@ import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.geometry.Pose2d;
-import edu.wpi.first.wpilibj.geometry.Rotation2d;
-import edu.wpi.first.wpiutil.math.MathUtil;
-import frc.robot.RobotContainer;
-import frc.robot.UtilityFunctions;
 import frc.robot.subsystems.base.common.Constant;
 import org.apache.commons.lang.math.DoubleRange;
 import org.apache.commons.lang.math.IntRange;
@@ -17,7 +12,6 @@ import org.apache.commons.lang.math.LongRange;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
-import static frc.robot.Constants.FieldGeometry.RED_OUTER_POWER_PORT_LOCATION;
 
 public class Utils {
 
@@ -57,14 +51,6 @@ public class Utils {
         return value;
     }
 
-    public static double map(double x, double in_min, double in_max, double out_min, double out_max) {
-        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-    }
-
-    public static double constrainedMap(double x, double in_min, double in_max, double out_min, double out_max) {
-        return MathUtil.clamp(map(x, in_min, in_max, out_min, out_max), out_min, out_max);
-    }
-
     public static void configAllFalcons(FalconConfiguration configurations, TalonFX... falcons) {
         for (TalonFX falcon : falcons) {
             falcon.configAllSettings(configurations.motorConfigs);
@@ -81,22 +67,6 @@ public class Utils {
             falcon.config_kF(0, configurations.getPidSet()[3]);
 
         }
-    }
-
-    /**
-     * Calculates turret angle to inner or outer port.
-     *
-     * @param currentPosition Current robot pose
-     * @param innerPort       Aim to inner port
-     * @return Turret angle
-     */
-    public static double calculateTurretAngle(Pose2d currentPosition, boolean innerPort) {
-        Pose2d targetLocation = UtilityFunctions.getPortLocation(innerPort);
-        double deltaY = targetLocation.getTranslation().getY() - currentPosition.getTranslation().getY();
-        double deltaX = targetLocation.getTranslation().getX() - currentPosition.getTranslation().getX();
-        double angle = Math.toDegrees(currentPosition.getRotation().getRadians() - Math.atan2(deltaY, deltaX));
-        if (angle < 0) angle += 360;
-        return angle;
     }
 
     /**
@@ -173,37 +143,6 @@ public class Utils {
 //            if (bClass.isEmpty()) continue; // Class isn't present
 //            Utils.replaceFields(aClass, bClass.get());
         }
-    }
-
-    public static Pose2d getRobotPoseFromX(double x, double rotationDegrees) {
-        if (!VisionModule.targetSeen()) return null;
-        double relativeTurretAngle = -(RobotContainer.turret.getAngle() + VisionModule.getVisionAngle()) - rotationDegrees; //Turret angle and vision angle should be positive, but because the auto works counter clockwize, we flip the value.
-        Pose2d newPose = new Pose2d(
-                UtilityFunctions.getPortLocation(false).getTranslation().getX() - x,
-                UtilityFunctions.getPortLocation(false).getTranslation().getY() - Math.tan(Math.toRadians(relativeTurretAngle)) * x,
-                Rotation2d.fromDegrees(rotationDegrees)
-        );
-        System.out.println(String.format("Reset with: %s | %s | %s | (%s, %s)", RobotContainer.turret.getAngle(), VisionModule.getVisionAngle(), rotationDegrees, newPose.getTranslation().getX(), newPose.getTranslation().getY()));
-        return newPose;
-    }
-
-    public static Pose2d getSimplePoseFromDistance(double rotationDegrees, double rawDistance) {
-        double relativeTurretAngle = rotationDegrees - RobotContainer.turret.getAngle();
-        return new Pose2d(
-                UtilityFunctions.getPortLocation(false).getTranslation().getX() - Math.cos(Math.toRadians(relativeTurretAngle)) * rawDistance,
-                UtilityFunctions.getPortLocation(false).getTranslation().getY() - Math.sin(Math.toRadians(relativeTurretAngle)) * rawDistance,
-                Rotation2d.fromDegrees(rotationDegrees)
-        );
-    }
-
-    /**
-     * returns the distance from the robot to the outer port
-     *
-     * @param robotPose
-     * @return
-     */
-    public static double localizationDistanceToPort(Pose2d robotPose) {
-        return robotPose.getTranslation().getDistance(RED_OUTER_POWER_PORT_LOCATION.getTranslation());
     }
 
     /**
