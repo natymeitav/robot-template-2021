140c8049a39f62df6101848c239546ec02f79651
Barel Shlidor <barel@shlidor.org>
2020-06-23 16:54:18 +0300

    Add retention and target to range constant annotation

--- src/main/java/frc/robot/subsystems/base/common/RangeConstant.java
+++ src/main/java/frc/robot/subsystems/base/common/RangeConstant.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems.base.common;
 
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface RangeConstant {
     double minDouble() default 0.;
     double maxDouble() default 0.;
