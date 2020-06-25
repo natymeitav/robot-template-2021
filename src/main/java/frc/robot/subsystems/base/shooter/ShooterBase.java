aa9df1661f11fe1c346ee893602b2dd150ff1f57
Barel Shlidor <barel@shlidor.org>
2020-06-25 14:38:39 +0300

    Add documentation to ShooterBase and remove Drivetrain class

--- src/main/java/frc/robot/subsystems/base/shooter/ShooterBase.java
+++ src/main/java/frc/robot/subsystems/base/shooter/ShooterBase.java
@@ -13,19 +13,50 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  */
 
 public abstract class ShooterBase extends SubsystemBase {
+
+    /**
+     * Get the current speed of the shooter (in <b>rpm</b>).
+     *
+     * @return the speed of the shooter in rpm.
+     */
     public abstract double getSpeed();
 
+    /**
+     * Set the speed of the shooter.
+     *
+     * @param speed the rotations per second of the shooter.
+     */
     public abstract void setSpeed(double speed);
 
-    public abstract boolean isShooterReady();
+    /**
+     * @return whether or not the shooter is fast enough to shoot.
+     */
+    public abstract boolean isReady();
 
+    /**
+     * @return whether or not the shooter is shooting.
+     */
     public abstract boolean isShooting();
 
+    /**
+     * Approximate the velocity you need to apply in order to shoot as correct as possible.
+     *
+     * @param distance the distance away from the target.
+     * @return the calculated velocity to get to the target in <b>rps</b>.
+     */
+    public abstract double approximateVelocity(double distance);
+
+    /**
+     * Set the power of the shooter.
+     *
+     * @param power the power you want to apply
+     */
     public abstract void setPower(double power);
 
+    /**
+     * Stop the shooter, so it won't move.
+     */
     public void stop() {
         setPower(0);
     }
-
-
 }
