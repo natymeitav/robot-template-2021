b17a920bfb823ad6edf6f94241bd98d69758fc53
Barel Shlidor <barel@shlidor.org>
2020-06-25 14:50:08 +0300

    Add documentation to IntakeBase class

--- src/main/java/frc/robot/subsystems/base/intake/IntakeBase.java
+++ src/main/java/frc/robot/subsystems/base/intake/IntakeBase.java
@@ -2,16 +2,39 @@ package frc.robot.subsystems.base.intake;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * @author Barel
+ * @version 1.0
+ * @since 2020-off
+ * <p>
+ * The class contains various methods that every {@code Intake} <b>should</b> implement.
+ * <p>
+ * The purpose of this class is to provide guidelines and restrict what important to us and what not.
+ */
+
 public abstract class IntakeBase extends SubsystemBase {
 
+    /**
+     * @return whether or not the shooter is opened.
+     */
     public abstract boolean isOpen();
 
+    /**
+     * @return whether or not the shooter is closed.
+     */
     public abstract boolean isClose();
 
+    /**
+     * Move the intake to the desired position.
+     *
+     * @param open whether you want to open the intake or not.
+     */
     public abstract void setPosition(boolean open);
 
-    public abstract void togglePosition(); //auto change
-
+    /**
+     * Move the intake based on the current position of the intake.
+     */
+    public abstract void togglePosition();
 
     /**
      * Returns the reading from the potentiometer through the talon. Note, this value is an integer,
@@ -21,8 +44,16 @@ public abstract class IntakeBase extends SubsystemBase {
      */
     public abstract int getSensorValue();
 
+    /**
+     * Move the wheel of the intake.
+     *
+     * @param power the power you want to apply on the intake.
+     */
     public abstract void powerWheels(double power);
 
+    /**
+     * Stop the wheels of the intake from moving.
+     */
     public void stopWheels() {
         powerWheels(0);
     }
