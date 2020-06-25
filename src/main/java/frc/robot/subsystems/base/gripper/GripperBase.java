1d4355f4e855e6daba142134742cdf2e96976472
Barel Shlidor <barel@shlidor.org>
2020-06-25 12:07:15 +0300

    Add methods to GripperBase class

--- src/main/java/frc/robot/subsystems/base/gripper/GripperBase.java
+++ src/main/java/frc/robot/subsystems/base/gripper/GripperBase.java
@@ -3,4 +3,25 @@ package frc.robot.subsystems.base.gripper;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public abstract class GripperBase extends SubsystemBase {
+
+    public abstract double getDistance();
+
+    public abstract boolean isObjectInside();
+
+    public abstract double getRightSpeed();
+
+    public abstract void setRightSpeed(double speed);
+
+    public abstract double getLeftSpeed();
+
+    public abstract void setLeftSpeed(double speed);
+
+    public void setSpeed(double leftSpeed, double rightSpeed) {
+        setLeftSpeed(leftSpeed);
+        setRightSpeed(rightSpeed);
+    }
+
+    public void setSpeed(double speed) {
+        setSpeed(speed, speed);
+    }
 }
