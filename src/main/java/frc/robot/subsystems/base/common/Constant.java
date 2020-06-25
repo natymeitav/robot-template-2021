87e7afb3c61f842a3c9add25413d1dc9d2dcfad3
Barel Shlidor <barel@shlidor.org>
2020-06-25 16:37:33 +0300

    Add documentation to Constant annotation

--- src/main/java/frc/robot/subsystems/base/common/Constant.java
+++ src/main/java/frc/robot/subsystems/base/common/Constant.java
@@ -1,11 +1,39 @@
 package frc.robot.subsystems.base.common;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+/**
+ * Indicates that a field is intended to override a field value.
+ * <p>
+ * The class use java reflection to replace the values at runtime,
+ * <b>NOTE: when you use this annotation on primitive object type, you need to use CONST() method.</b>
+ *
+ * @author Barel
+ * @version 1.0
+ * @see org.apache.commons.lang3.ObjectUtils#CONST(int)
+ * @see org.apache.commons.lang3.ObjectUtils#CONST(double)
+ * @see org.apache.commons.lang3.ObjectUtils#CONST(long)
+ * @see frc.robot.utilities.Utils#swapConstants(Class)
+ * @since 2020-off
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Constant {
+    /**
+     * @return a value of double object.
+     */
     double doubleVal() default 0.0;
+
+    /**
+     * @return a value of integer object.
+     */
     int intVal() default 0;
+
+    /**
+     * @return a value of long object.
+     */
     long longVal() default 0L;
 }
