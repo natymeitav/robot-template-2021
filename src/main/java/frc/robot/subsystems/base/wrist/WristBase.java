1ada3f9d33dcd9d6f0f10c12f2f12283b6d194e4
Barel Shlidor <barel@shlidor.org>
2020-06-25 12:17:12 +0300

    Add methods to WristBase class

--- src/main/java/frc/robot/subsystems/base/wrist/WristBase.java
+++ src/main/java/frc/robot/subsystems/base/wrist/WristBase.java
@@ -3,4 +3,26 @@ package frc.robot.subsystems.base.wrist;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public abstract class WristBase extends SubsystemBase {
+
+    public abstract double getAngle();
+
+    public abstract void setAngle(double angle);
+
+    public abstract double getSpeed();
+
+    public abstract void setSpeed(double speed);
+
+    public abstract void setPower(double power);
+
+    public abstract boolean atPeak();
+
+    public abstract boolean atClosed();
+
+    public abstract int getVelocity();
+
+    public abstract double getMaxAngle();
+
+    public abstract double getMinAngle();
+
+    public abstract double applyForceByAngle(double angle);
 }
