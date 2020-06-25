f358fd92a0c16aa170ad65f95c57f593a1ff0afa
Barel Shlidor <barel@shlidor.org>
2020-06-25 12:26:40 +0300

    Add (more) methods to Localizable interface

--- src/main/java/frc/robot/subsystems/base/common/Localizable.java
+++ src/main/java/frc/robot/subsystems/base/common/Localizable.java
@@ -5,8 +5,18 @@ import edu.wpi.first.wpilibj.geometry.Pose2d;
 public interface Localizable {
 
     double getHeading();
+
     double getCCWHeading();
 
+    double getLeftDistance();
+
+    double getRightDistance();
+
+    void resetPose();
+
+    double getAngle();
+
     Pose2d getPose();
+
     void setPose(Pose2d desiredPose);
 }
