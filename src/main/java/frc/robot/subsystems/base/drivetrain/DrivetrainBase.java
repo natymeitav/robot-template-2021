238ea09ed225b6c51e26091a9dbcd8bf3995dc3d
Barel Shlidor <barel@shlidor.org>
2020-06-25 12:26:23 +0300

    Add (more) methods to DrivetrainBase class

--- src/main/java/frc/robot/subsystems/base/drivetrain/DrivetrainBase.java
+++ src/main/java/frc/robot/subsystems/base/drivetrain/DrivetrainBase.java
@@ -11,9 +11,22 @@ public abstract class DrivetrainBase extends SubsystemBase {
 
     public abstract void setPower(double leftPower, double rightPower);
 
+    public void setSpeed(double leftSpeed, double rightSpeed) {
+        setLeftSpeed(leftSpeed);
+        setRightSpeed(rightSpeed);
+    }
+
     public abstract double getLeftVelocity();
 
     public abstract double getRightVelocity();
+
+    public abstract double getLeftSpeed();
+
+    public abstract void setLeftSpeed(double speed);
+
+    public abstract double getRightSpeed();
+
+    public abstract void setRightSpeed(double speed);
 
     public abstract void setBrake(boolean brake);
 
