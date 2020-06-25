093a693806c82dd9d8293b8165a3f7b07183cfff
Barel Shlidor <barel@shlidor.org>
2020-06-25 17:18:08 +0300

    Add documentation to WithBlockPositions interface

--- src/main/java/frc/robot/subsystems/base/common/WithBlockPositions.java
+++ src/main/java/frc/robot/subsystems/base/common/WithBlockPositions.java
@@ -1,12 +1,41 @@
 package frc.robot.subsystems.base.common;
 
+/**
+ * Add methods that indicated that the subsystem have block areas.
+ * <p>
+ *
+ * @author Barel
+ * @version 1.0
+ * @since 2020-off
+ */
 public interface WithBlockPositions {
 
-    boolean inCorrectRange();
+    /**
+     * @return whether the subsystem in valid position.
+     */
+    boolean inCorrectRange(double position);
 
-    boolean inDeadZone();
+    /**
+     * @return whether the subsystem in invalid position.
+     */
+    boolean inDeadZone(double position);
 
-    double getNearestPosition(double targetAngle, double currentPosition, double minPos, double maxPos);
+    /**
+     * Set the velocity you need to apply in valid path in order to come into the target position.
+     *
+     * @param targetPos       the target position.
+     * @param currentPosition the current position.
+     * @param minPos          the minimum position.
+     * @param maxPos          the maximum position.
+     * @return
+     */
+    double getNearestPosition(double targetPos, double currentPosition, double minPos, double maxPos);
 
+    /**
+     * Check the validity of the position.
+     *
+     * @param position the position to check.
+     * @return whether the position is valid.
+     */
     boolean checkValidity(double position);
 }
