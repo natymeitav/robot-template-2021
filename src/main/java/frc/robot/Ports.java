dcb9cf38a5e7fcf478953e3c7a0df0c4f7194543
yael <yael.peleg4@gmail.com>
2020-07-28 14:12:27 +0300

    Add Ports for the Intake Subsystem

---
+++ src/main/java/frc/robot/Ports.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+
+public class Ports {
+    public static class Intake{
+        public static final int MOTOR = 20;
+        public static final boolean IS_INVERTED = false;
+    }
+}
