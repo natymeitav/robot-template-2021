9c62d41b6ba71c957171d42e83996f1c80d99d96
Unknown <orel.david1502@gmail.com>
2020-07-30 21:32:57 +0300

    Add file templates

---
+++ .idea/fileTemplates/Command.java
@@ -0,0 +1,34 @@
+package ${PACKAGE_NAME};
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+/**
+ *
+ */
+public class ${NAME} extends CommandBase{
+
+    public ${NAME}() {
+
+        }
+
+    // Called just before this Command runs the first time
+    public void initialize() {
+
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    public void execute() {
+
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    public boolean isFinished() {
+
+        return false;
+
+    }
+
+    // Called once after isFinished returns true
+    public void end(boolean interrupted) {
+
+    }
+}
