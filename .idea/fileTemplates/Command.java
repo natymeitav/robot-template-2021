50e4ca42387eed74b7f186081f2466c4e4692f2d
orel david aboukasis <orel.david1502@gmail.com>
2020-07-31 10:11:26 +0300

    Add override annotation

--- .idea/fileTemplates/Command.java
+++ .idea/fileTemplates/Command.java
@@ -10,16 +10,19 @@ public class ${NAME} extends CommandBase{
 
         }
 
+    @Override
     // Called just before this Command runs the first time
     public void initialize() {
 
     }
 
+    @Override
     // Called repeatedly when this Command is scheduled to run
     public void execute() {
 
     }
 
+    @Override
     // Make this return true when this Command no longer needs to run execute()
     public boolean isFinished() {
 
@@ -27,6 +30,7 @@ public class ${NAME} extends CommandBase{
 
     }
 
+    @Override
     // Called once after isFinished returns true
     public void end(boolean interrupted) {
 
